62e03f8ea10fe32bd033bc20e5c295cd0350f436
Andy <andreas.jan.van.der.meulen@gmail.com>
2025-11-12 09:54:18 +0100

    La til funksjon for å lage en liste med doblede verdier

--- telling.py
+++ telling.py
@@ -1,1 +1,3 @@
-
+def doblet(liste: list[float]) -> list[float]:
+    """Returnerer en ny liste med alle verdiene doblet"""
+    return list(map(lambda x: x*2, liste))
--- test_telling.py
+++ test_telling.py
@@ -1,1 +1,6 @@
+import pytest
+from telling import doblet
 
+@pytest.mark.parametrize("liste,expected", [([1, 2, 3], [2, 4, 6]), ([-1, 2, 0], [-2, 4, 0])])
+def test_dobling(liste, expected):
+    assert doblet(liste) == expected
